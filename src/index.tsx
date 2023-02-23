e64b440740046ad79fd2cbf1fdb7f31ea2fd174c
Takashi364364 <takashitakahashi114514@gmail.com>
2023-02-24 05:11:08 +0900

    headを変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
+      <Helmet>
+        <title>きょうのメシ</title>
+      </Helmet>
+    </HelmetProvider>
     <Provider store={store}>
       <App />
     </Provider>
